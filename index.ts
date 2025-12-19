b36b743427acc3cceb6b0962b5a14a69e4918f14
Mike <mpotapov.learning@gmail.com>
2025-12-19 22:07:58 +0700

    Add base chat with ollama

--- index.ts
+++ index.ts
@@ -1,13 +1,41 @@
+import ollama from 'ollama'
+import type {Message} from 'ollama'
 
+class Chat {
+    private model = 'gemma3:12b'
+    private messages:  Message[] = []
 
+   async send(text: string) {
+       this.messages.push({ role: 'user', content: text })
+
+        const response = await ollama.chat({
+            stream: true,
+            model: this.model,
+            messages: this.messages
+        })
+
+       let answer = "";
+       for await (const part of response) {
+           answer += part.message.content;
+           process.stdout.write(part.message.content)
+       }
+
+       process.stdout.write("\n")
+
+       this.messages.push({role: 'assistant', content: answer})
+    }
+}
 
 async function main() {
+    const chat = new Chat();
+
     for await (const line of console) {
         const input = line.trim();
-        if (input === "exit") break
+        if (input === "exit") {
+            break
+        }
 
-        console.log(`Response: ${input.toUpperCase()}`);
-        console.write("> ");
+        await chat.send(input);
     }
 }
 
