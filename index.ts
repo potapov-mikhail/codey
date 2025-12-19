0bb7f7d0071233f7894087e144cc682528a69c53
Mike <mpotapov.learning@gmail.com>
2025-12-19 21:45:42 +0700

    Add REPL

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+
+
+
+async function main() {
+    for await (const line of console) {
+        const input = line.trim();
+        if (input === "exit") break
+
+        console.log(`Response: ${input.toUpperCase()}`);
+        console.write("> ");
+    }
+}
+
+main()
